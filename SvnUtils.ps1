90489b2c6c5d67ed10361e7d1e7fc2c4140e9ff5
Daniel Welsh <dewey@earthling.net>
2013-01-21 15:54:43 -0800

    Incorporate changes from posh-git so the two play nice together

--- SvnUtils.ps1
+++ SvnUtils.ps1
@@ -78,3 +78,7 @@ function tsvn {
   }
 }
 
+function Get-AliasPattern($exe) {
+  $aliases = @($exe) + @(Get-Alias | where { $_.Definition -eq $exe } | select -Exp Name)
+  "($($aliases -join '|'))"
+}
