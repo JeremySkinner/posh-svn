af699b7ae7fd14948b1d81d232193bc298569ffc
fahsig <fahsig@redlogix.de>
2016-03-04 10:45:53 +0100

    ADDED: External check ADDED: Check if repo available then do a svn status -u (takes way longer)

--- SvnPrompt.ps1
+++ SvnPrompt.ps1
@@ -2,29 +2,37 @@ $global:SvnPromptSettings = New-Object PSObject -Property @{
     BeforeText                = ' ['
     BeforeForegroundColor     = [ConsoleColor]::Yellow
     BeforeBackgroundColor     = $Host.UI.RawUI.BackgroundColor
-    
+
     AfterText                 = ']'
     AfterForegroundColor      = [ConsoleColor]::Yellow
     AfterBackgroundColor      = $Host.UI.RawUI.BackgroundColor
-    
+
     BranchForegroundColor     = [ConsoleColor]::Cyan
     BranchBackgroundColor     = $Host.UI.RawUI.BackgroundColor
-    
+
     RevisionForegroundColor   = [ConsoleColor]::DarkGray
     RevisionBackgroundColor   = $Host.UI.RawUI.BackgroundColor
-    
+
     WorkingForegroundColor    = [ConsoleColor]::Yellow
     WorkingBackgroundColor    = $Host.UI.RawUI.BackgroundColor
+
+    ExternalStatusSymbol      = [char]0x2190 # arrow right
+    ExternalForegroundColor   = [ConsoleColor]::DarkMagenta
+    ExternalBackgroundColor   = $Host.UI.RawUI.BackgroundColor
+
+    IncomingStatusSymbol      = [char]0x2193 # Down arrow
+    IncomingForegroundColor   = [ConsoleColor]::Red
+    IncomingBackgroundColor   = $Host.UI.RawUI.BackgroundColor
 }
 
 function Write-SvnStatus($status) {
     if ($status) {
         $s = $global:SvnPromptSettings
-       
+
         Write-Prompt $s.BeforeText -NoNewline -BackgroundColor $s.BeforeBackgroundColor -ForegroundColor $s.BeforeForegroundColor
         Write-Prompt $status.Branch -NoNewline -BackgroundColor $s.BranchBackgroundColor -ForegroundColor $s.BranchForegroundColor
         Write-Prompt "@$($status.Revision)" -NoNewline -BackgroundColor $s.RevisionBackgroundColor -ForegroundColor $s.RevisionForegroundColor
-        
+
         if($status.Added) {
           Write-Prompt " +$($status.Added)" -NoNewline -BackgroundColor $s.WorkingBackgroundColor -ForegroundColor $s.WorkingForegroundColor
         }
@@ -34,19 +42,28 @@ function Write-SvnStatus($status) {
         if($status.Deleted) {
           Write-Prompt " -$($status.Deleted)" -NoNewline -BackgroundColor $s.WorkingBackgroundColor -ForegroundColor $s.WorkingForegroundColor
         }
-        
+
         if ($status.Untracked) {
           Write-Prompt " ?$($status.Untracked)" -NoNewline -BackgroundColor $s.WorkingBackgroundColor -ForegroundColor $s.WorkingForegroundColor
         }
-                
+
         if($status.Missing) {
            Write-Prompt " !$($status.Missing)" -NoNewline -BackgroundColor $s.WorkingBackgroundColor -ForegroundColor $s.WorkingForegroundColor
         }
-      
+
         if($status.Conflicted) {
           Write-Prompt " C$($status.Conflicted)" -NoNewLine -BackgroundColor $s.WorkingBackgroundColor -ForegroundColor $s.WorkingForegroundColor
         }
-      
+
+        if($status.Incoming) {
+          Write-Prompt " $($s.IncomingStatusSymbol)$($status.Incoming)" -NoNewLine -BackgroundColor $s.IncomingBackgroundColor -ForegroundColor $s.IncomingForegroundColor
+          Write-Prompt "@$($status.IncomingRevision)" -NoNewline -BackgroundColor $s.RevisionBackgroundColor -ForegroundColor $s.RevisionForegroundColor
+        }
+
+        if($status.External) {
+          Write-Prompt " $($s.ExternalStatusSymbol)$($status.External)" -NoNewLine -BackgroundColor $s.ExternalBackgroundColor -ForegroundColor $s.ExternalForegroundColor
+        }
+
         Write-Prompt $s.AfterText -NoNewline -BackgroundColor $s.AfterBackgroundColor -ForegroundColor $s.AfterForegroundColor
     }
 }
--- SvnUtils.ps1
+++ SvnUtils.ps1
@@ -22,44 +22,79 @@ function Get-SvnStatus {
   if(IsSvnDirectory) {
     $untracked = 0
     $added = 0
+    $ignored = 0
     $modified = 0
+    $replaced = 0
     $deleted = 0
     $missing = 0
     $conflicted = 0
+    $external = 0
+    $obstructed = 0
+    $incoming = 0
+    $incomingRevision = 0
     $branchInfo = Get-SvnBranchInfo
-    
-    svn status | foreach {
-      $char = $_[0]
-      switch($char) {
-         'A' { $added++ }
-         'C' { $conflicted++ }
-         'D' { $deleted++ }
-         'M' { $modified++ }
-         'R' { $modified++ }
-         '?' { $untracked++ }
-         '!' { $missing++ }
-      }
+    $info = Get-SvnInfo
+    $hostName = ([System.Uri]$info[2].Replace("URL: ", "")).Host #URL: http://svnserver/trunk/test
+
+    if (Test-Connection -computername $hostName -Quiet -Count 1 -BufferSize 1) {
+        $status = svn status -u --ignore-externals
+    } else {
+        $status = svn status --ignore-externals
     }
-    
+
+    foreach($line in $status) {
+        if ($line.StartsWith("Status"))
+        {
+            $incomingRevision = [Int]$line.Replace("Status against revision:", "")
+        }
+        else
+        {
+            switch($line[0]) {
+                'A' { $added++; break; }
+                'C' { $conflicted++; break; }
+                'D' { $deleted++; break; }
+                'I' { $ignored++; break; }
+                'M' { $modified++; break; }
+                'R' { $replaced++; break; }
+                'X' { $external++; break; }
+                '?' { $untracked++; break; }
+                '!' { $missing++; break; }
+                '~' { $obstructed++; break; }
+            }
+            switch($line[4]) {
+                'X' { $external++; break; }
+            }
+            switch($line[6]) {
+                'C' { $conflicted++; break; }
+            }
+            switch($line[8]) {
+                '*' { $incoming++; break; }
+            }
+        }
+    }
+
     return @{"Untracked" = $untracked;
                "Added" = $added;
-               "Modified" = $modified;
+               "Modified" = $modified + $replaced;
                "Deleted" = $deleted;
                "Missing" = $missing;
-               "Conflicted" = $conflicted;
+               "Conflicted" = $conflicted + $obstructed;
+               "External" = $external;
+               "Incoming" = $incoming
                "Branch" = $branchInfo.Branch;
-               "Revision" = $branchInfo.Revision;}
+               "Revision" = $branchInfo.Revision;
+               "IncomingRevision" = $incomingRevision;}
    }
 }
 
 function Get-SvnBranchInfo {
   if(IsSvnDirectory) {
-    $info = svn info
+    $info = Get-SvnInfo
     $url = $info[3].Replace("Relative URL: ^/", "") #Relative URL: ^/trunk/test
     $revision = $info[6].Replace("Revision: ", "") #Revision: 1234
-    
+
     $pathBits = $url.Split("/", [StringSplitOptions]::RemoveEmptyEntries)
-    
+
     if($pathBits[0] -eq "trunk") {
       $branch =  "trunk";
     }
@@ -76,23 +111,22 @@ function tsvn {
     if($args[0] -eq "help") {
       #I don't like the built in help behaviour!
       $tsvnCommands.keys | sort | % { write-host $_ }
-          
-      return    
+
+      return
     }
 
     $newArgs = @()
     $newArgs += "/command:" + $args[0]
-    
+
     $cmd = $tsvnCommands[$args[0]]
     if($cmd -and $cmd.useCurrentDirectory) {
        $newArgs += "/path:."
     }
-    
+
     if($args.length -gt 1) {
       $args[1..$args.length] | % { $newArgs += $_ }
     }
-      
+
     tortoiseproc $newArgs
   }
 }
-
